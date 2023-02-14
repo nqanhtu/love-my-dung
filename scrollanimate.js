a1bc42d8ba0a32634135c8cde8c81e0123cee752
Tu Nguyen <nqanhtu@outlook.com>
2023-02-14 12:43:21 +0700

    finish

--- scrollanimate.js
+++ scrollanimate.js
@@ -1,14 +1,17 @@
-const observer = new IntersectionObserver((entries) => {
-  entries.forEach((entry) => {
-    console.log(entry);
-    if (entry.isIntersecting) {
-      entry.target.classList.add('show');
-    } else {
-      entry.target.classList.remove('show');
-    }
-  });
-});
+const observer = new IntersectionObserver(
+  (entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add('show');
+      } else {
+        entry.target.classList.remove('show');
+      }
+    });
+  },
+  {
+    rootMargin: '-20px',
+  }
+);
 
 const hiddenElements = document.querySelectorAll('.hidden');
-console.log(hiddenElements);
 hiddenElements.forEach((el) => observer.observe(el));
